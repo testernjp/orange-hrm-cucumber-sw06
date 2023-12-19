0923ef9cce644c7015e8b2dac92e842ce409f7b0
testernjp <nishitjpatel13@gmail.com>
2023-12-19 14:06:43 +0000

    selenium-hw-wk06

---
+++ src/test/java/com/orangehrmlive/demo/runner/RunCukeTest.java
@@ -0,0 +1,15 @@
+package com.orangehrmlive.demo.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/orangehrmlive/demo/steps",
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        tags = "@regression"
+)
+public class RunCukeTest extends AbstractTestNGCucumberTests {
+}
